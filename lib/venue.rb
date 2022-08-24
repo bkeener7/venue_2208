e9f5a80a626e397ed9af182c8d0d4a49c40b0281
Bryan Keener <bkeener7@gmail.com>
2022-08-24 09:48:47 -0600

    complete iteration 3

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,18 +1,29 @@
 class Venue
-    attr_reader :name, :capacity, :patrons
+    attr_reader :name, :capacity, :patrons, :number_patrons
     def initialize(name, capacity)
         @name = name
         @capacity = capacity
         @patrons = []
+        @number_patrons = 0
+    end
+
+    def new_person
+        @number_patrons += 1
     end
 
     def add_patron(patron)
         @patrons << patron
+        new_person
     end
     
     def yell_at_patrons
-        return patrons.each do |patron|
+        patrons.each do |patron|
            patron.upcase!
         end
+    end
+
+    def over_capacity?
+        return false if @number_patrons <= 4
+        true
     end
 end
