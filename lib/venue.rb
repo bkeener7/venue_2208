470920fab308d0dcd7cec366508f691d909eb683
Bryan Keener <bkeener7@gmail.com>
2022-08-24 09:31:20 -0600

    complete iteration 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -9,4 +9,10 @@ class Venue
     def add_patron(patron)
         @patrons << patron
     end
+    
+    def yell_at_patrons
+        return patrons.each do |patron|
+           patron.upcase!
+        end
+    end
 end
